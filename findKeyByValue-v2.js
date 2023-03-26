49256b0f7160f188b07b828ce292d392ddf3e1a5
Ahmed <ahmed91alhaj@gmail.com>
2023-03-26 08:41:59 -0400

    implemented findKeyByValue function

---
+++ findKeyByValue-v2.js
@@ -0,0 +1,27 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
+    return true;
+  } else {
+    console.log(` ❗❗❗ Assertion Failed: ${actual} !== ${expected}`);
+    return false;
+  }
+};
+
+const findKeyByValue = function (obj, value) {
+  for (let key of Object.keys(obj)) {
+    if (obj[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
