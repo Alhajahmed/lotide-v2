50339d7de07428111a58f2c8110a2869ed14debb
Ahmed <ahmed91alhaj@gmail.com>
2023-03-26 04:48:33 -0400

    imlemented countOnly function

--- head-v2.js
+++ head-v2.js
@@ -3,7 +3,7 @@ const assertEqual = function (actual, expected) {
     console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
     return true;
   } else {
-    console.log(` ❗❗❗ Assertion Passed: ${actual} !== ${expected}`);
+    console.log(` ❗❗❗ Assertion Failed: ${actual} !== ${expected}`);
     return false;
   }
 };
