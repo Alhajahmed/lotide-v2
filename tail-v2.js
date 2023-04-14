2b5f48747e3d75f92a1f1a9b7673e9248202b9ac
Ahmed <ahmed91alhaj@gmail.com>
2023-04-14 06:21:13 -0400

    Refactored tail

--- tail-v2.js
+++ tail-v2.js
@@ -1,13 +1,3 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
-    return true;
-  } else {
-    console.log(` ❗❗❗ Assertion Failed: ${actual} !== ${expected}`);
-    return false;
-  }
-};
-
 const tail = function (array) {
   if (array.length === 0) {
     return [];
@@ -16,16 +6,4 @@ const tail = function (array) {
   }
 };
 
-// Test assertions
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-// Test Case: Check the returned array for an array with only one element
-const resultOne = tail(["Hello"]);
-assertEqual(resultOne.length, 0); // expected output: []
-
-// Test Case: Check the returned array for an empty array
-const resultEmpty = tail([]);
-assertEqual(resultEmpty.length, 0); // expected output: []
+module.exports = tail;
---
+++ test/tailTest-v2.js
@@ -0,0 +1,18 @@
+const assertEqual = require("../assertEqual-v2");
+const tail = require("../tail-v2");
+
+// Tail test assertions
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+// Test Case: Check the returned array for an array with only one element
+const resultOne = tail(["Hello"]);
+assertEqual(resultOne.length, 0); // expected output: []
+
+// Test Case: Check the returned array for an empty array
+const resultEmpty = tail([]);
+assertEqual(resultEmpty.length, 0); // expected output: []
