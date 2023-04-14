174a726b72c6e8a194a5d79ee6ea468d725e8d31
Ahmed <ahmed91alhaj@gmail.com>
2023-04-14 06:41:35 -0400

    Refactored assertArraysEqual

--- assertArraysEqual-v2.js
+++ assertArraysEqual-v2.js
@@ -1,3 +1,5 @@
+const eqArrays = require("./eqArrays-v2");
+
 const assertArraysEqual = function (firstArray, secArray) {
   if (eqArrays(firstArray, secArray)) {
     console.log(`✅ ✅ ✅ Assertion Passed: [${firstArray}] === [${secArray}]`);
@@ -6,18 +8,4 @@ const assertArraysEqual = function (firstArray, secArray) {
   }
 };
 
-const eqArrays = function (firstArray, secArray) {
-  if (firstArray.length !== secArray.length) {
-    return false;
-  }
-  for (let i = 0; i < firstArray.length; i++) {
-    if (firstArray[i] !== secArray[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-// Test assertions
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
+module.exports = assertArraysEqual;
