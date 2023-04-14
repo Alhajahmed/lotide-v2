ba6e0eb973372b1cb1981882e814fd4d0fb245e8
Ahmed <ahmed91alhaj@gmail.com>
2023-04-14 06:48:14 -0400

    Refactored middle

--- middle-v2.js
+++ middle-v2.js
@@ -1,22 +1,3 @@
-const assertArraysEqual = function (firstArray, secArray) {
-  if (eqArrays(firstArray, secArray)) {
-    console.log(`✅ ✅ ✅ Assertion Passed: [${firstArray}] === [${secArray}]`);
-  } else {
-    console.log(`❗❗❗ Assertion Failed: [${firstArray}] !== [${secArray}]`);
-  }
-};
-const eqArrays = function (firstArray, secArray) {
-  if (firstArray.length !== secArray.length) {
-    return false;
-  }
-  for (let i = 0; i < firstArray.length; i++) {
-    if (firstArray[i] !== secArray[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
 const middle = function (array) {
   const middleElements = [];
   middleIndex = Math.floor(array.length / 2);
@@ -30,10 +11,4 @@ const middle = function (array) {
   return middleElements;
 };
 
-// Test assertions
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
---
+++ test/middleTest-v2.js
@@ -0,0 +1,10 @@
+const assertArraysEqual = require("../assertArraysEqual-v2");
+const middle = require("../middle-v2");
+
+// Test assertions
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
